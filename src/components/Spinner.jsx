0d5127d8455ea8a5e50cbb840805d1a3689f4ee5
David Yartey <yarteyd@gmail.com>
2019-10-09 21:36:41 +0000

    added a spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Spinner extends React.Component {
+  render() {
+    return (
+      <div class='d-flex justify-content-center'>
+        <div class='spinner-border' role='status'>
+          <span class='sr-only'>Loading...</span>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import TaskItem from './TaskItem';
 import AddTask from './AddTask';
 import axios from 'axios';
+import Spinner from '../Spinner';
 
 export default class Todo extends React.Component {
   state = {
-    tasks: []
+    tasks: [],
+    isLoading: 0
   };
-
   componentDidMount() {
     const url = 'http://127.0.0.1:8000/api/todos';
-    axios.get(url).then(res => this.setState({ tasks: res.data.data }));
+    this.setState({ isLoading: 1 });
+    axios
+      .get(url)
+      .then(res => this.setState({ tasks: res.data.data, isLoading: 0 }));
   }
   deleteTask = taskId => {
     const tasks = this.state.tasks.filter(task => {
@@ -52,15 +56,19 @@ export default class Todo extends React.Component {
       <div>
         {/* <hr /> */}
         <AddTask addTask={this.addTask}></AddTask>
-        {/* <h4 className='text pb-4'>
-          Tasks{' '}
+        {/* <h4 className='text pb-4'>Tasks </h4> */}
+        <div className='text pb-0 text-center bold'>
+          Tasks
           <small className='pl-0 pr-2 text-small'>({this.countTasks()})</small>
-        </h4> */}
-        <p className={this.getStyleForYourTasks()}>
-          {this.countTasks() === 0
-            ? 'Your have not added any tasks yet.'
-            : 'All Tasks'}
-        </p>
+        </div>
+        {this.state.isLoading === 1 ? (
+          <Spinner></Spinner>
+        ) : (
+          <p className={this.getStyleForYourTasks()}>
+            {this.countTasks() === 0 ? 'You have not added any tasks yet.' : ''}
+          </p>
+        )}
+
         <div className='bg-outline'>
           {this.state.tasks
             .map(task => {
